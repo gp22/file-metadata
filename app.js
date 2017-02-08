b20f5210f2b929c699f8dd315bb6c750d62e1e1c
gp22 <gp22@tutanota.com>
2017-02-08 14:49:41 +0700

    add home.ejs

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ file upload. When you submit a file, you will receive the filesize in
 bytes in the JSON response.
 */
 'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('home'));
+
+// use process.env.PORT for compatibility with heroku
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
