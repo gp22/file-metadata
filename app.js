f24d80e2c463978702074f53f1a1dd8f4c7e1fae
gp22 <gp22@tutanota.com>
2017-02-08 14:00:15 +0700

    initial commit, framework setup

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+File Metadata Microservice
+
+Deployed to: `https://gp22-filemetadata.herokuapp.com/`
+
+This simple service let's you submit form data that includes a
+file upload. When you submit a file, you will receive the filesize in
+bytes in the JSON response.
+*/
+'use strict';
