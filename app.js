1db0d1db45bc591bb7d79cca4cca98c02f4d68a4
gp22 <gp22@tutanota.com>
2017-02-08 10:11:30 +0000

    add file-upload function and JSON response

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ bytes in the JSON response.
 'use strict';
 
 const express = require('express');
+const multer = require('multer');
 const app = express();
+const upload = multer();
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('home'));
+
+app.post('/file-upload', upload.single('upload'), (req, res) => {
+    const response = {
+        "size": req.file.size
+    }
+    res.send(response);
+});
 
 // use process.env.PORT for compatibility with heroku
 app.listen(process.env.PORT || 3000, () => console.log('Server started'));
